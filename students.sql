2119564c429a91dfcc65ed79642ea195a94fc137
jumeyla <142509110+jumeyla@users.noreply.github.com>
2024-10-10 16:37:14 +0800

    added new file

--- students.sql
+++ students.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 06, 2024 at 04:29 PM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
+-- Generation Time: Oct 10, 2024 at 10:35 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,7 +35,7 @@ CREATE TABLE `attendance` (
   `time` time DEFAULT NULL,
   `attendance_status` enum('Present','Absent','Late') DEFAULT NULL,
   `section` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `attendance`
@@ -43,7 +43,7 @@ CREATE TABLE `attendance` (
 
 INSERT INTO `attendance` (`id`, `student_id`, `date`, `subject_id`, `time`, `attendance_status`, `section`) VALUES
 (3, 6, '2024-10-06', NULL, '14:41:50', 'Present', NULL),
-(4, 7, '2024-10-06', NULL, '14:42:00', 'Present', NULL);
+(4, 7, '2024-10-06', NULL, '14:42:00', '', NULL);
 
 -- --------------------------------------------------------
 
@@ -57,7 +57,7 @@ CREATE TABLE `students` (
   `section` varchar(50) DEFAULT NULL,
   `guardian_email` varchar(100) DEFAULT NULL,
   `guardian_phone` varchar(15) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `students`
@@ -77,7 +77,7 @@ CREATE TABLE `student_subjects` (
   `id` int(11) NOT NULL,
   `student_id` int(11) NOT NULL,
   `subject_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `student_subjects`
@@ -96,7 +96,7 @@ INSERT INTO `student_subjects` (`id`, `student_id`, `subject_id`) VALUES
 CREATE TABLE `subjects` (
   `id` int(11) NOT NULL,
   `subject_name` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `subjects`
@@ -106,6 +106,29 @@ INSERT INTO `subjects` (`id`, `subject_name`) VALUES
 (2, 'math'),
 (3, 'english'),
 (6, 'science');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `institution` varchar(100) NOT NULL,
+  `department` enum('CABECS','CHAP','CASE','COE','BED') DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `institution`, `department`, `created_at`) VALUES
+(1, 'jumeyla', '$2y$10$vw92iG20TNky89V61tbhN.shmkEXJO1Y0WNPKrHpmaDem5p5Ee5cK', 'CSAB', 'CABECS', '2024-10-09 16:38:42'),
+(2, 'mama mo', '$2y$10$EJiIvKIcubR4E14ahUcXIOfJznnm5411rXZ5LVjdVLi.gJGgpmqxO', 'CSAB', NULL, '2024-10-10 05:30:12');
 
 --
 -- Indexes for dumped tables
@@ -140,6 +163,13 @@ ALTER TABLE `subjects`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -166,6 +196,12 @@ ALTER TABLE `student_subjects`
 --
 ALTER TABLE `subjects`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
