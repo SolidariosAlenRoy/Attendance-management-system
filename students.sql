52dcc5e62950a7c4bcd26110c552471c34d9d77f
Radgelwashere <142951831+SamanoRajelJohann@users.noreply.github.com>
2024-10-13 22:26:42 +0800

    Updated SQL

--- students.sql
+++ students.sql
@@ -25,6 +25,11 @@ CREATE TABLE `attendance` (
   CONSTRAINT `attendance_ibfk_3` FOREIGN KEY (`section`) REFERENCES `students` (`section`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+INSERT INTO `attendance` (`id`, `student_id`, `date`, `subject_id`, `time`, `attendance_status`, `section`) VALUES
+(22,	15,	'2024-10-13',	1,	'14:45:57',	'Absent',	NULL),
+(23,	15,	'2024-10-13',	2,	'14:45:57',	'Absent',	NULL),
+(24,	15,	'2024-10-13',	3,	'14:45:57',	'Absent',	NULL),
+(25,	15,	'2024-10-13',	4,	'14:45:57',	'Absent',	NULL);
 
 DROP TABLE IF EXISTS `students`;
 CREATE TABLE `students` (
@@ -37,6 +42,8 @@ CREATE TABLE `students` (
   KEY `section` (`section`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+INSERT INTO `students` (`id`, `student_name`, `section`, `guardian_email`, `guardian_phone`) VALUES
+(15,	'Ced',	'1',	'nilda@gmail.com',	'091231762412');
 
 DROP TABLE IF EXISTS `student_subjects`;
 CREATE TABLE `student_subjects` (
@@ -50,6 +57,11 @@ CREATE TABLE `student_subjects` (
   CONSTRAINT `student_subjects_ibfk_2` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+INSERT INTO `student_subjects` (`id`, `student_id`, `subject_id`) VALUES
+(29,	15,	1),
+(30,	15,	2),
+(31,	15,	3),
+(32,	15,	4);
 
 DROP TABLE IF EXISTS `subjects`;
 CREATE TABLE `subjects` (
@@ -58,6 +70,11 @@ CREATE TABLE `subjects` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+INSERT INTO `subjects` (`id`, `subject_name`) VALUES
+(1,	'English'),
+(2,	'Math'),
+(3,	'Science'),
+(4,	'Filipino');
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
@@ -71,5 +88,8 @@ CREATE TABLE `users` (
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+INSERT INTO `users` (`id`, `username`, `password`, `institution`, `department`, `created_at`) VALUES
+(1,	'jumeyla',	'$2y$10$vw92iG20TNky89V61tbhN.shmkEXJO1Y0WNPKrHpmaDem5p5Ee5cK',	'CSAB',	'CABECS',	'2024-10-09 16:38:42'),
+(2,	'mama mo',	'$2y$10$EJiIvKIcubR4E14ahUcXIOfJznnm5411rXZ5LVjdVLi.gJGgpmqxO',	'CSAB',	NULL,	'2024-10-10 05:30:12');
 
--- 2024-10-13 07:01:08
+-- 2024-10-13 14:25:23
